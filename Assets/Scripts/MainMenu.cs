575cfc821cb997ec42386c30c62e901a6eca4124
gabrielhebert <ghebert10@gmail.com>
2014-04-27 22:22:03 -0400

    assss

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,11 +2,14 @@
 using System.Collections;
 
 public class MainMenu : MonoBehaviour {
-
+	public GameObject obj;
 	void OnGUI () {
 		GUI.backgroundColor = Color.yellow;
 		if (GUI.Button (new Rect (Screen.width*0.29f, Screen.height*0.58f, Screen.width*0.2f, Screen.height*0.15f), "PLAY")) {
+			NetworkStuff other =(NetworkStuff) obj.GetComponent(typeof(NetworkStuff));
+			other.ConnectToServer();
 			Application.LoadLevel("MainGame");
+
 		}
 	}
 }
---
+++ Assets/Scripts/NetworkStuff.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkStuff : MonoBehaviour {
+	public string stringToEdit = "Ip address";
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad(transform.gameObject);
+	}
+	void OnGUI () {
+		GUI.backgroundColor = Color.yellow;
+		stringToEdit = GUI.TextField (new Rect (10, 10, 200, 20), stringToEdit, 25);
+
+	}
+
+	public void ConnectToServer()
+	{
+		Debug.Log("MES PENISS");
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
