7fea1b8a77f12ad821a717067bfd6587ef73d47a
Drackhyo <drackhyo@gmail.com>
2014-04-27 23:06:03 -0400

    stuff

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class MainMenu : MonoBehaviour {
 	public GameObject obj;
+	public string stringToEdit = "Ip address";
 	void OnGUI () {
 		GUI.backgroundColor = Color.yellow;
+
+		stringToEdit = GUI.TextField (new Rect (10, 10, 200, 20), stringToEdit, 25);
 		if (GUI.Button (new Rect (Screen.width*0.29f, Screen.height*0.58f, Screen.width*0.2f, Screen.height*0.15f), "PLAY")) {
 			NetworkStuff other =(NetworkStuff) obj.GetComponent(typeof(NetworkStuff));
-			other.ConnectToServer();
+			other.ConnectToServer(stringToEdit);
 			Application.LoadLevel("MainGame");
 
 		}
--- Assets/Scripts/NetworkStuff.cs
+++ Assets/Scripts/NetworkStuff.cs
@@ -2,18 +2,14 @@
 using System.Collections;
 
 public class NetworkStuff : MonoBehaviour {
-	public string stringToEdit = "Ip address";
+
 	// Use this for initialization
 	void Start () {
 		DontDestroyOnLoad(transform.gameObject);
 	}
-	void OnGUI () {
-		GUI.backgroundColor = Color.yellow;
-		stringToEdit = GUI.TextField (new Rect (10, 10, 200, 20), stringToEdit, 25);
 
-	}
 
-	public void ConnectToServer()
+	public void ConnectToServer(string ip)
 	{
 		Debug.Log("MES PENISS");
 	}
